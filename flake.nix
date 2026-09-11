7e1e38e9e31cdbde9fcd2620c7d370d011f2aef1
h4rl <h4rldev@proton.me>
2026-09-11 20:24:01 +0200

    feat: arm64 linux builds, gpg-signed checksums

--- flake.nix
+++ flake.nix
@@ -5,24 +5,62 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = inputs: let
-    system = "x86_64-linux";
-    pkgs = import inputs.nixpkgs {inherit system;};
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    eachSystem = nixpkgs.lib.genAttrs systems;
+
+    cargo = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+    version = cargo.package.version;
+
+    mkConjure = pkgs: buildType:
+      pkgs.rustPlatform.buildRustPackage {
+        pname = "conjure-${buildType}";
+        inherit version;
+
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+        inherit buildType;
+
+        nativeBuildInputs = [pkgs.pkg-config];
+        buildInputs = [pkgs.openssl];
+      };
   in {
-    devShells.${system}.default = pkgs.mkShell {
-      name = "conjure";
+    packages = eachSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      conjure-debug = mkConjure pkgs "debug";
+      conjure-release = mkConjure pkgs "release";
+      default = mkConjure pkgs "release";
+    });
 
-      buildInputs = with pkgs; [
-        pkg-config
-        openssl
-        cmake
+    devShells = eachSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = pkgs.mkShell {
+        name = "conjure";
 
-        ### For the test project.
-        mold
-        libuv
-        openssl.dev
-        zlib
-      ];
-    };
+        buildInputs = with pkgs; [
+          pkg-config
+          openssl
+          cmake
+
+          ### For the test project.
+          mold
+          libuv
+          openssl.dev
+          zlib
+        ];
+      };
+    });
   };
 }
